1ed54ee41eca230f4ee006cc5886c7749e37b256
Fatima Zahra Zeghli <120262100+idajikuu@users.noreply.github.com>
2023-05-20 13:17:01 +0100

    Update pca.py

--- pca.py
+++ pca.py
@@ -1,41 +1,58 @@
-#Ihssane Aoune TP ACP
 import numpy as np
 
-def acp(X, k):
+def calculate_eigen(X):
+    n = X.shape[0]
+    eigenvalues = np.zeros(n)
+    eigenvectors = np.zeros((n, n))
 
-    # Calculer la matrice de Covariance 
-    moy_vec = np.mean(X, axis=0)
-    X_centre = X - moy_vec
-    matrice_cov = np.dot(X_centre.T, X_centre) / (X.shape[0] - 1)
+    for i in range(n):
+        v = np.random.rand(n)
+        v = v / np.linalg.norm(v)
 
-    # Calculer les vecteurs et valeurs propres :
-    val_propres, vec_props = np.linalg.eig(matrice_cov)
+        for _ in range(100):
+            v_new = np.dot(X, v)
+            v_new = v_new / np.linalg.norm(v_new)
 
-    # Composantes principales:
-    indices = np.argsort(val_propres)[::-1]  # Tri des valeurs propres du plus grand au plus petit
-    selected_vec_props = vec_props[:, indices[:k]]
+            if np.linalg.norm(v_new - v) < 1e-6:
+                break
 
-    #Projection des données
-    donnee_reduite = np.dot(X_centre, selected_vec_props)
+            v = v_new
 
-    return donnee_reduite
+        eigenvalues[i] = np.dot(v, np.dot(X, v))
+        eigenvectors[:, i] = v
+
+        X = X - eigenvalues[i] * np.outer(v, v)
+
+    return eigenvalues, eigenvectors
 
 
-num_samples = int(input("Entrer le nombre des échantillons : "))
-num_features = int(input("Entrer le nombre des features: "))
+def pca(X, k):
+    X_centered = X - np.mean(X, axis=0)
+    covariance_matrix = np.dot(X_centered.T, X_centered) / (X.shape[0] - 1)
 
-print("La matrice des données:")
+    eigenvalues, eigenvectors = calculate_eigen(covariance_matrix)
+
+    indices = np.argsort(eigenvalues)[::-1]
+    selected_eigenvectors = eigenvectors[:, indices[:k]]
+
+    reduced_data = np.dot(X_centered, selected_eigenvectors)
+
+    return reduced_data
+
+
+num_samples = int(input("Enter the number of samples: "))
+num_features = int(input("Enter the number of features: "))
+
+print("Data matrix:")
 data = np.zeros((num_samples, num_features))
 for i in range(num_samples):
     for j in range(num_features):
-        data[i][j] = float(input(f"Valuer de donnée :[{i}][{j}]: "))
+        data[i][j] = float(input(f"Enter value for data[{i}][{j}]: "))
 
-# Appliquer ACP avec deux composantes principales k=2 
+k = int(input("Enter the number of principal components (k): "))
+reduced_data = pca(data, k)
 
-k = 2
-donnee_reduite = acp(data, k)
-
-print("Data shape d'origine:", data.shape)
-print("Data shape réduit:", donnee_reduite.shape)
-print("Données réduites:")
-print(donnee_reduite)
+print("Original data shape:", data.shape)
+print("Reduced data shape:", reduced_data.shape)
+print("Reduced data:")
+print(reduced_data)
